5c3f713c5711c9c5fff78f55e0631f1d8fe7e9ed
Annelie Östelid <Annelie.ostelid@outlook.com>
2025-02-17 11:31:02 +0100

    used queryselectors

---
+++ template/scripts/utils/domUtils.js
@@ -0,0 +1,8 @@
+//Hämtat section för mina trailers
+const trailersContainerRef = document.querySelector(`.trailer`);
+
+//section för rekommenderade filmer
+const cardContainerRef = document.querySelector(`#cardContainer`);
+
+//detaljer för enskild film
+const movieInformationRef = document.querySelector(`#movieInformation`);
