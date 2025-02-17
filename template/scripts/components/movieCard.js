c3f14e5f8ced5416abf410234ed368745defaeb1
Annelie Östelid <Annelie.ostelid@outlook.com>
2025-02-17 12:03:45 +0100

    export getFavorites from DOM too movieCard

--- template/scripts/components/movieCard.js
+++ template/scripts/components/movieCard.js
@@ -1,6 +1,8 @@
 //skapande av varje film-kort, stor och liten variant?
 // innerhtml frånm variabel
 
+import { getFavorites } from "../utils/storage";
+
 // knapp för att lägga till favorit med add-eventlistener.
 
 // Funktioner för att visa innehåll
@@ -11,7 +13,7 @@ export function showAllCards(container) {
 
 export function showFavorites(container) {
   // Hämta favoriter från localStorage och visa dem
-  const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+  getFavorites();
   container.innerHTML = ""; // Rensa befintligt innehåll
 
   favorites.forEach((favorite) => {
@@ -30,12 +32,3 @@ export function showSearchResults(container) {
   // Visa sökresultat, kanske genom att hämta data från en sökning
   container.innerHTML = "<div>Sökresultat här...</div>";
 }
-
-//Hämtat section för mina trailers
-const trailersContainerRef = document.querySelector(`.trailer`);
-
-//section för rekommenderade filmer, återanvänds på index, favorites och search
-const cardContainerRef = document.querySelector(`#cardContainer`);
-
-//detaljer för enskild film
-const movieInformationRef = document.querySelector(`#movieInformation`);
--- template/scripts/script.js
+++ template/scripts/script.js
@@ -1,16 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const container = trailersContainerRef;
+import {
+  showFavorites,
+  showMovieDetails,
+  showSearchResults,
+} from "./components/movieCard";
 
+document.addEventListener("DOMContentLoaded", () => {
   if (
     window.location.pathname === "/" ||
     window.location.pathname === "/index.html"
   ) {
     console.log("index.html");
+    showAllCards(cardContainerRef);
   } else if (window.location.pathname === "/favorites.html") {
     console.log("favorites.html");
+    showFavorites(cardContainerRef);
   } else if (window.location.pathname === "/movie.html") {
     console.log("movie.html");
+    // behållare för enskild film
+    showMovieDetails(movieInformationRef);
   } else if (window.location.pathname === "/search.html") {
     console.log("search.html");
+    showSearchResults(cardContainerRef);
   }
 });
